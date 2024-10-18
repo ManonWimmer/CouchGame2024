18fb129d4b2c2c51451ff55573ad42a6059f3f87
Manon Wimmer <manonwimmer30@gmail.com>
2024-10-18 09:32:53 +0200

    [ADD] Create project & change .gitignore

---
+++ CouchGame2024/Source/CouchGame2024/CouchGame2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CouchGame2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CouchGame2024, "CouchGame2024" );
