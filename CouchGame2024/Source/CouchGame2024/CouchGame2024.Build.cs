1ec664fd2aa11f32c830607939f338086f7f9b7a
Manon Wimmer <manonwimmer30@gmail.com>
2024-10-27 14:35:50 +0100

    [UPDATE] Test switch Grappling Hook from BP to CPP Working <3

--- CouchGame2024/Source/CouchGame2024/CouchGame2024.Build.cs
+++ CouchGame2024/Source/CouchGame2024/CouchGame2024.Build.cs
@@ -10,7 +10,7 @@ public class CouchGame2024 : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "DeveloperSettings" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "GeometryFramework", "GeometryFramework", "LocalMultiplayer" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "GeometryFramework", "GeometryFramework", "LocalMultiplayer", "CableComponent" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
