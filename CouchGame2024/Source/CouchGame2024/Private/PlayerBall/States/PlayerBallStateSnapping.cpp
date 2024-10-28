204b0504a382f60d1ba217775e35d44482d7feb2
Corentinrmt <corentin.remot@gmail.com>
2024-10-28 14:06:22 +0100

    [ADD] setup player state snapping

---
+++ CouchGame2024/Source/CouchGame2024/Private/PlayerBall/States/PlayerBallStateSnapping.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerBall/States/PlayerBallStateSnapping.h"
+
+
+// Sets default values for this component's properties
+UPlayerBallStateSnapping::UPlayerBallStateSnapping()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPlayerBallStateSnapping::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPlayerBallStateSnapping::TickComponent(float DeltaTime, ELevelTick TickType,
+                                             FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ CouchGame2024/Source/CouchGame2024/Public/PlayerBall/States/PlayerBallStateSnapping.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerBall/PlayerBallState.h"
+#include "PlayerBallStateSnapping.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class COUCHGAME2024_API UPlayerBallStateSnapping : public UPlayerBallState
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPlayerBallStateSnapping();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
