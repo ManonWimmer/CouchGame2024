2a471905ef208376c33a451b1b53c9ee7465d758
Manon Wimmer <manonwimmer30@gmail.com>
2024-10-28 11:05:47 +0100

    [UPDATE] Grappling Hook integration in Player ball V1

--- CouchGame2024/Source/CouchGame2024/Public/PlayerBall/PlayerBall.h
+++ CouchGame2024/Source/CouchGame2024/Public/PlayerBall/PlayerBall.h
@@ -14,6 +14,7 @@ class UPlayerBallStateMachine;
 class USphereComponent;
 class UFloatingPawnMovement;
 class UStaticMeshComponent;
+class UCableComponent;
 
 UCLASS()
 class COUCHGAME2024_API APlayerBall : public APawn
@@ -65,6 +66,13 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<USphereComponent> SphereCollision;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USphereComponent> GrapplingSphereCollision;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UCableComponent> GrapplingCable;
+	
 #pragma endregion
 
 #pragma region StateMachine
@@ -230,7 +238,55 @@ public:
 
 	UPROPERTY()
 	TObjectPtr<APlayerBall> GrappledPlayerBall;	// Ball grappled to this playerBall
-	
+
+	UPROPERTY()
+	float CableLength = 0.f;
+
+	UPROPERTY()
+	FVector HookPoint = FVector(0.f, 0.f, 0.f);
+
+	UPROPERTY()
+	float CurrentGrapplingAngularVelocity = 0.f;
+
+	UPROPERTY()
+	float CurrentGrapplingAngle = 0.f;
+
+	UPROPERTY()
+	bool IsGrappling = false;
+
+	UPROPERTY()
+	float LastAngle = 0.f;
+
+	UPROPERTY()
+	float AngleRotate = 0.f;
+
+	UPROPERTY()
+	FVector ReleaseDirection = FVector(0.f, 0.f, 0.f);
+
+	UPROPERTY()
+	FVector GrapplingOffset = FVector(0.f, 0.f, 0.f);
+
+	UPROPERTY(EditAnywhere)
+	float GrapplingDamping = 0.99f;
+
+	UPROPERTY(EditAnywhere)
+	float GrapplingForce = 0.1f;
+
+	UPROPERTY(EditAnywhere)
+	float GrapplingReleaseForce = 250.f;
+
+	UPROPERTY(EditAnywhere)
+	float MinCableDistance = 200.f;
+
+	UPROPERTY(EditAnywhere)
+	float MaxCableDistance = 500.f;
+
+	UPROPERTY(EditAnywhere)
+	float MoreOrLessCablePerFrame = 10.f;
+
+	UPROPERTY(EditAnywhere)
+	float StartGrapplingForceFactorWhenAlreadyMoving = 0.001f;
+
 #pragma endregion
 
 #pragma region Grappled
