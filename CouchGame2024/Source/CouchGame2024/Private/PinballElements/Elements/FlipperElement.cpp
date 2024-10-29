0b301290b658cde0f08daa82702200e66ddb35a7
Corentinrmt <corentin.remot@gmail.com>
2024-10-29 09:58:36 +0100

    [FIX] snapping Ignore Z Axis

--- CouchGame2024/Source/CouchGame2024/Private/PinballElements/Elements/FlipperElement.cpp
+++ CouchGame2024/Source/CouchGame2024/Private/PinballElements/Elements/FlipperElement.cpp
@@ -3,22 +3,12 @@
 
 #include "PinballElements/Elements/FlipperElement.h"
 
-#include "Components/BoxComponent.h"
-
 
 // Sets default values
 AFlipperElement::AFlipperElement()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	FlipperBoxTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Trigger"));
-	FlipperMeshRoot = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Root"));
-	FlipperMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-
-	FlipperBoxTrigger->SetupAttachment(RootComponent);
-	FlipperMeshRoot->SetupAttachment(FlipperBoxTrigger);
-	FlipperMesh->SetupAttachment(FlipperMeshRoot);
 }
 
 // Called when the game starts or when spawned
@@ -32,54 +22,5 @@ void AFlipperElement::BeginPlay()
 void AFlipperElement::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
-}
-
-void AFlipperElement::TriggerElement()
-{
-	Super::TriggerElement();
-
-	ActivateFlip();
-}
-
-EPinballElementID AFlipperElement::GetElementID()
-{
-	return EPinballElementID::Flipper;
-}
-
-void AFlipperElement::ActivateFlip()
-{
-	if (IsFlipping)	return;
-
-	IsFlipping = true;
-}
-
-void AFlipperElement::HandleFlipRotation(float DeltaTime)
-{
-	if (IsFlipping)
-	{
-		if (CurrentRotationAngle < FlipperMaxRotationAngle)
-		{
-			// Rotate the flipper towards the maximum angle
-
-			CurrentRotationAngle = FMath::FInterpTo(CurrentRotationAngle, FlipperMaxRotationAngle, DeltaTime, RotationSpeed);
-			
-			CurrentRotationAngle = FMath::Clamp(CurrentRotationAngle, 0.0f, FlipperMaxRotationAngle);
-
-			// Apply the rotation to the flipper mesh
-			FRotator NewRotation = FRotator(CurrentRotationAngle, 0, 0);
-			FlipperMeshRoot->SetRelativeRotation(NewRotation);
-		}
-	}
-	else
-	{
-		CurrentRotationAngle = FMath::FInterpTo(CurrentRotationAngle, 0.f, DeltaTime, RotationSpeed);
-			
-		CurrentRotationAngle = FMath::Clamp(CurrentRotationAngle, 0.0f, FlipperMaxRotationAngle);
-
-		// Apply the rotation to the flipper mesh
-		FRotator NewRotation = FRotator(CurrentRotationAngle, 0, 0);
-		FlipperMeshRoot->SetRelativeRotation(NewRotation);
-	}
 }
 
--- CouchGame2024/Source/CouchGame2024/Public/PinballElements/Elements/FlipperElement.h
+++ CouchGame2024/Source/CouchGame2024/Public/PinballElements/Elements/FlipperElement.h
@@ -6,8 +6,6 @@
 #include "PinballElements/PinballElement.h"
 #include "FlipperElement.generated.h"
 
-class UBoxComponent;
-
 UCLASS()
 class COUCHGAME2024_API AFlipperElement : public APinballElement
 {
@@ -17,17 +15,6 @@ public:
 	// Sets default values for this actor's properties
 	AFlipperElement();
 
-private:
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<UStaticMeshComponent> FlipperMesh;
-
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<USceneComponent> FlipperMeshRoot;
-
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<UBoxComponent> FlipperBoxTrigger;
-	
-	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -35,25 +22,4 @@ protected:
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	virtual void TriggerElement() override;
-	
-	virtual EPinballElementID GetElementID() override;
-
-private:
-	UFUNCTION(BlueprintCallable)
-	void ActivateFlip();
-
-	UFUNCTION()
-	void HandleFlipRotation(float DeltaTime);
-	
-	UPROPERTY()
-	bool IsFlipping;
-	
-	float CurrentRotationAngle = 0.0f;
-
-	UPROPERTY(EditAnywhere, Category = "Flipper Settings")
-	float FlipperMaxRotationAngle = 45.0f;
-	UPROPERTY(EditAnywhere, Category = "Flipper Settings")
-	float RotationSpeed = 90.0f;
 };
