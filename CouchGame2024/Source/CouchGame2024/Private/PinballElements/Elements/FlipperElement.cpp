257c365333d11606dce9ea09abf5622baecbaa9b
Corentinrmt <corentin.remot@gmail.com>
2024-10-29 10:08:34 +0100

    [ADD] flipper element setup

--- CouchGame2024/Source/CouchGame2024/Private/PinballElements/Elements/FlipperElement.cpp
+++ CouchGame2024/Source/CouchGame2024/Private/PinballElements/Elements/FlipperElement.cpp
@@ -3,24 +3,93 @@
 
 #include "PinballElements/Elements/FlipperElement.h"
 
+#include "Components/BoxComponent.h"
+#include "PlayerBall/PlayerBall.h"
+
+
+void AFlipperElement::OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	TObjectPtr<APlayerBall> OtherBall = Cast<APlayerBall>(OtherActor);
+
+	if (OtherBall != nullptr)
+	{
+		// Flip if not retourné, sinon reviens en position normale
+		FlipUp();
+	}
+}
 
 // Sets default values
 AFlipperElement::AFlipperElement()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+
+	BoxTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxTrigger"));
+	FlipperMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FlipperMesh"));
+
+	BoxTrigger->SetupAttachment(RootComponent);
+	FlipperMesh->SetupAttachment(BoxTrigger);
+
+	if (BoxTrigger != nullptr)
+	{
+		BoxTrigger->OnComponentBeginOverlap.AddDynamic(this, &AFlipperElement::OnTriggerBeginOverlap);
+	}
 }
+
 
 // Called when the game starts or when spawned
 void AFlipperElement::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Rot Init
+	OriginalRotation = FRotator(0.f, 0.f, 0.f);
+	FlippedRotation = FRotator(0.f, 0.f, 45.f); // 45 deg
+	bIsFlipped = false;
+
+	OriginalRotation = FlipperMesh->GetRelativeRotation();
 }
 
 // Called every frame
 void AFlipperElement::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (bIsFlipped &&  FlipperMesh->GetRelativeRotation() != FlippedRotation)
+	{
+		FlipperMesh->SetRelativeRotation(FMath::RInterpTo(FlipperMesh->GetRelativeRotation(), FlippedRotation, DeltaTime, FlipUpSpeed));
+	}
+	else if (!bIsFlipped && FlipperMesh->GetRelativeRotation() != OriginalRotation)
+	{
+		FlipperMesh->SetRelativeRotation(FMath::RInterpTo(FlipperMesh->GetRelativeRotation(), OriginalRotation, DeltaTime, FlipDownSpeed));
+
+	}
+}
+
+EPinballElementID AFlipperElement::GetElementID()
+{
+	return EPinballElementID::Flipper;
+}
+
+void AFlipperElement::TriggerElement()
+{
+	Super::TriggerElement();
+
+}
+
+void AFlipperElement::FlipUp()
+{
+	if (bIsFlipped)	return;
+
+	bIsFlipped = true;
+}
+
+void AFlipperElement::FlipDown()
+{
+	if (!bIsFlipped)	return;
+
+	bIsFlipped = false;
 }
 
--- CouchGame2024/Source/CouchGame2024/Public/PinballElements/Elements/FlipperElement.h
+++ CouchGame2024/Source/CouchGame2024/Public/PinballElements/Elements/FlipperElement.h
@@ -6,12 +6,17 @@
 #include "PinballElements/PinballElement.h"
 #include "FlipperElement.generated.h"
 
+class UBoxComponent;
+
 UCLASS()
 class COUCHGAME2024_API AFlipperElement : public APinballElement
 {
 	GENERATED_BODY()
 
 public:
+	UFUNCTION()
+	void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                           const FHitResult& SweepResult);
 	// Sets default values for this actor's properties
 	AFlipperElement();
 
@@ -22,4 +27,39 @@ protected:
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+#pragma region Components
+public:
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UBoxComponent> BoxTrigger;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> FlipperMesh;
+
+#pragma endregion 
+	
+public:
+
+	virtual EPinballElementID GetElementID() override;
+
+	virtual void TriggerElement() override;
+
+	UFUNCTION(BlueprintCallable)
+	void FlipUp();
+	
+	UFUNCTION(BlueprintCallable)
+	void FlipDown();
+
+	UPROPERTY(EditAnywhere)
+	FRotator OriginalRotation;
+	UPROPERTY(EditAnywhere)
+	FRotator FlippedRotation;
+	bool bIsFlipped;
+
+	UPROPERTY(EditAnywhere)
+	float FlipUpSpeed = 20.f;
+
+	UPROPERTY(EditAnywhere)
+	float FlipDownSpeed = 20.f;
 };
