b0e2ace1c9ec5e2c2b032048f3e6d93cb9496ef4
Corentinrmt <corentin.remot@gmail.com>
2024-11-04 15:22:16 +0100

    [ADD] part 1 setup component PowerUpPlayerBallComponent

--- CouchGame2024/Source/CouchGame2024/Public/PlayerBall/PlayerBall.h
+++ CouchGame2024/Source/CouchGame2024/Public/PlayerBall/PlayerBall.h
@@ -404,6 +404,9 @@ private:
 #pragma region PowerUp
 
 public:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UPowerUpPlayerBallComponent> PowerUpComponent;
+	
 	EPowerUpID GetCurrentPowerUpCarried() const;
 
 	void SetPowerUpCarried(EPowerUpID PowerUpID);
