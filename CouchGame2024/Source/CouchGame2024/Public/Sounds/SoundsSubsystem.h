b28ef983c89f5d9d2ab907505f0f043b7bf8641f
Corentinrmt <corentin.remot@gmail.com>
2024-11-20 11:32:59 +0100

    [ADD] create soundsSubsystem + unlink size and mass in playerBall bp

---
+++ CouchGame2024/Source/CouchGame2024/Public/Sounds/SoundsSubsystem.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SoundsSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COUCHGAME2024_API USoundsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+};
