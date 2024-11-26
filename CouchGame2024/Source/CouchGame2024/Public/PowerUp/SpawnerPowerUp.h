d4bfaa0f5f7a6b30e89bfff3be07091b5dd6c2b0
Corentinrmt <corentin.remot@gmail.com>
2024-11-26 17:06:02 +0100

    [ADD] setup spawnerPower up part1

---
+++ CouchGame2024/Source/CouchGame2024/Public/PowerUp/SpawnerPowerUp.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PowerUpID.h"
+#include "GameFramework/Actor.h"
+#include "SpawnerPowerUp.generated.h"
+
+UCLASS()
+class COUCHGAME2024_API ASpawnerPowerUp : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASpawnerPowerUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	void SpawnSpecificPowerUp(EPowerUpID InPowerUpID);
+	
+	void SpawnRandomPowerUp();
+
+	EPowerUpID ChooseRandomPowerUp();
+	
+};
