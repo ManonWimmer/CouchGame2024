28d30c69ddd04cbb334efc5da943a2fb3682dd91
CorentinRmt <corentin.remot@gmail.com>
2024-11-27 00:10:24 +0100

    [UPDATE] set power class spawn in spawner

---
+++ CouchGame2024/Source/CouchGame2024/Public/PowerUp/PowerUpDataType.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PowerUpDataType.generated.h"
+
+class APowerUpSlippery;
+class APowerUpStrength;
+class APowerUpFreeze;
+/**
+ * 
+ */
+UCLASS()
+class COUCHGAME2024_API UPowerUpDataType : public UDataAsset
+{
+	GENERATED_BODY()
+
+
+public:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<APowerUpFreeze> PowerUpTypeFreeze;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<APowerUpStrength> PowerUpTypeStrength;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<APowerUpSlippery> PowerUpTypeSlippery;
+};
