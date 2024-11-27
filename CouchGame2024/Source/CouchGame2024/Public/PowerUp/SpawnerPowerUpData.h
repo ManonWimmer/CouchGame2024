9a03baa94b91697c67d9d8620be2e739aa94fd87
Corentinrmt <corentin.remot@gmail.com>
2024-11-27 15:04:58 +0100

    [UPDATE] customizable random cooldown spawner power up

---
+++ CouchGame2024/Source/CouchGame2024/Public/PowerUp/SpawnerPowerUpData.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SpawnerPowerUpData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COUCHGAME2024_API USpawnerPowerUpData : public UDataAsset
+{
+	GENERATED_BODY()
+};
