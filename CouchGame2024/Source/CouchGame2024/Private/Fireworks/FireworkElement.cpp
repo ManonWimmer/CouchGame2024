a3eca05439c39ad4caa6c2b4b34b990072ed04cf
CorentinRmt <corentin.remot@gmail.com>
2024-12-13 00:47:56 +0100

    [ADD] setup fireworks system

---
+++ CouchGame2024/Source/CouchGame2024/Private/Fireworks/FireworkElement.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Fireworks/FireworkElement.h"
+
+
+// Sets default values
+AFireworkElement::AFireworkElement()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFireworkElement::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFireworkElement::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFireworkElement::FireFirework()
+{
+	ReceiveOnFireFirework();
+}
+
+void AFireworkElement::StopFirework()
+{
+	
+}
+
---
+++ CouchGame2024/Source/CouchGame2024/Public/Fireworks/FireworksSubsystem.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "FireworksSubsystem.generated.h"
+
+class AFireworkElement;
+/**
+ * 
+ */
+UCLASS()
+class COUCHGAME2024_API UFireworksSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	
+	UFUNCTION()
+	void InitFireworkSubsystem();
+
+
+	UFUNCTION()
+	void FireAllFireworks();
+
+	UFUNCTION()
+	void StopAllFireworks();
+	
+private:
+	UPROPERTY()
+	TArray<AFireworkElement*> FireworkElements;
+};
